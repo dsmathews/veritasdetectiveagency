7c42d13d036f80cec2aa915cb893e724e19a7b0a
Darrell Mathews <smathewsbd@gmail.com>
2019-07-23 13:54:13 -0400

    full name of state for selected value

--- contactForm.js
+++ contactForm.js
@@ -24,6 +24,7 @@ $('document').ready(function (){
 
       console.log(dataCollection);
       console.table(dataCollection);
+      $('form').val('');
    }
    $("#submit").on("click", getInfo)
    })
