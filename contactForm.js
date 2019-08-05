da7c66a14b1a8868c2edf8430664d825ec4c599c
Darrell Mathews <smathewsbd@gmail.com>
2019-08-04 21:18:57 -0400

    removed google tag manager

--- contactForm.js
+++ contactForm.js
@@ -25,14 +25,25 @@ $('document').ready(function () {
          "Specific Situation": situation,
          "Description": description
       }
-      $.ajax({
-         type: "PUT",
-         URL: 'https://523u7qu7ui.execute-api.us-east-1.amazonaws.com/prod/contact',
-      })
-      $.post('https://523u7qu7ui.execute-api.us-east-1.amazonaws.com/prod/contact', dataCollection)
-         .then(
-            console.log(JSON)
-         )
+
+      // const errRecord = function(req, status, err) {
+      //    console.log('something went wrong', req, status, err);
+
+      // }
+
+      // const okRecord = function(res) {
+
+      // }
+
+      // $.ajax({
+      //    type: "PUT",
+      //    URL: 'https://523u7qu7ui.execute-api.us-east-1.amazonaws.com/prod/contact',
+      //    data: dataCollection,
+      //    datatype: JSON,
+      //    error: errRecord,
+      //    success: 
+      // })
+      $.post('https://523u7qu7ui.execute-api.us-east-1.amazonaws.com/prod/contact', dataCollection, function (res) {console.log(res)});
       console.log(dataCollection);
       console.table(dataCollection);
       $("#contactForm").trigger("reset")
